3cb38d3459fe6ca28ce13d9008efd74ea4ffa3d4
josar <josarroliga0943@outlook.es>
2022-11-06 17:57:50 -0600

    Font-family implementada, botones github funcionales.

---
+++ src/scripts/script.js
@@ -0,0 +1,23 @@
+const button_ExpedienteMedico = document.getElementsByClassName("github_button")[0]
+
+const button_Programatic = document.getElementsByClassName("github_button")[1]
+
+const button_Gestor = document.getElementsByClassName("github_button")[2]
+
+const button_Apple = document.getElementsByClassName("github_button")[3]
+
+button_ExpedienteMedico.addEventListener("click",function () {
+    window.open("https://github.com/Chocoyito/ExpedienteMedicoFinalizado")
+})
+
+button_Programatic.addEventListener("click",function () {
+    window.open("https://github.com/Chocoyito/Programatic")
+})
+
+button_Gestor.addEventListener("click",function () {
+    window.open("https://github.com/Chocoyito/SistemaControlEySBD")
+})
+
+button_Apple.addEventListener("click",function () {
+    window.open("https://github.com/Chocoyito/ApolHechizo")
+})
